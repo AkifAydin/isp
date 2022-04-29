6589df26acb678fade027cdc915687afec4148fb
akifn <akif.aydin@haw-hamburg.de>
2022-04-29 20:33:40 +0200

    idaStar

---
+++ src/de/hawhamburg/is/praktikum1/IDA_Star.java
@@ -0,0 +1,102 @@
+package de.hawhamburg.is.praktikum1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class IDA_Star {
+    public static void main(String[] args) {
+        // create graph
+        Node head = new Node(3);
+        head.g = 0;
+
+        Node n1 = new Node(2);
+        Node n2 = new Node(2);
+        Node n3 = new Node(2);
+
+        head.addBranch(1, n1);
+        head.addBranch(5, n2);
+        head.addBranch(2, n3);
+        n3.addBranch(1, n2);
+
+        Node n4 = new Node(1);
+        Node n5 = new Node(1);
+        Node target = new Node(0);
+
+        n1.addBranch(7, n4);
+        n2.addBranch(4, n5);
+        n3.addBranch(6, n4);
+
+        n4.addBranch(3, target);
+        n5.addBranch(1, n4);
+        n5.addBranch(3, target);
+
+        Node res = idaStarAlg(head, target);
+        printPath(res);
+    }
+    public static Node idaStarAlg(Node start, Node target){
+        Node root = start;
+        Node ziel = target;
+        PriorityQueue<Node> openList = new PriorityQueue<>();
+
+        start.f = start.g + start.calculateHeuristic(target);
+        openList.add(start);
+
+        while(!openList.isEmpty()){
+            // return first node
+            Node n = openList.peek();
+            if(n == target){
+                return n;
+            }
+
+            for(Node.Edge edge : n.neighbors){
+                Node m = edge.node;
+                double totalWeight = n.g + edge.weight;
+
+                if(!openList.contains(m) && !closedList.contains(m)){
+                    m.parent = n;
+                    m.g = totalWeight;
+                    m.f = m.g + m.calculateHeuristic(target);
+                    openList.add(m);
+                } else {
+                    if(totalWeight < m.g){
+                        m.parent = n;
+                        m.g = totalWeight;
+                        m.f = m.g + m.calculateHeuristic(target);
+
+                        if(closedList.contains(m)){
+                            closedList.remove(m);
+                            openList.add(m);
+                        }
+                    }
+                }
+            }
+
+            openList.remove(n);
+            closedList.add(n);
+        }
+        return null;
+    }
+
+    public static void printPath(Node target){
+        Node n = target;
+
+        if(n==null)
+            return;
+
+        List<Integer> ids = new ArrayList<>();
+
+        while(n.parent != null){
+            ids.add(n.id);
+            n = n.parent;
+        }
+        ids.add(n.id);
+        Collections.reverse(ids);
+
+        for(int id : ids){
+            System.out.print(id + " ");
+        }
+        System.out.println();
+    }
+}
